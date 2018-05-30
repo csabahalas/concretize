b54b14cb4ff35fa35bb79288fa32e7d9ccd2292d
Ralph Tang <r33tang@uwaterloo.ca>
2018-05-30 01:06:46 -0400

    Add MNIST with quantized concrete RVs

--- concretize/__main__.py
+++ concretize/__main__.py
@@ -1,22 +1,14 @@
 import argparse
 
+from torchvision.datasets import MNIST
+from torchvision import transforms
 import torch
+import torch.nn as nn
 import torch.optim as optim
 
 import model as mod
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--batch_size", default=1, type=int)
-    parser.add_argument("--lr", default=1E-3, type=float)
-    parser.add_argument("--dev_every", default=10, type=int)
-    parser.add_argument("--device", default=0, type=int, help="-1 for CPU, all else for GPU id")
-    parser.add_argument("--epochs", default=30, type=int)
-    parser.add_argument("--momentum", default=0, type=float)
-    args = parser.parse_args()
-    device = f"{'cuda' if args.device >= 0 else 'cpu'}:{args.device}"
-    device = torch.device(device)
-
+def run_example(args, device):
     model = mod.MultiTuckerExampleModel().to(device)
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
     for epoch in range(args.epochs):
@@ -29,6 +21,59 @@ def main():
             model.eval()
             loss = 0.5 * model.mse(1024)
             print(f"Epoch: {epoch} Loss: {loss.item():.5} Params: {[p.detach().cpu().sigmoid().numpy() for p in model.parameters()]}")
+
+def print_eval(model, criterion, loader, device, name="dev"):
+    model.eval()
+    total_accuracy = 0
+    total_loss = 0
+    total_count = 0
+    for inputs, labels in loader:
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+        scores = model(inputs).detach()
+        loss = criterion(scores, labels).detach()
+        total_accuracy += (torch.max(scores, 1)[1] == labels).float().sum()
+        total_loss += loss.item()
+        total_count += scores.size(0)
+    print(f"{name} accuracy: {total_accuracy / total_count:.4} loss: {total_loss / total_count:.4}")
+
+def run_mnist(args, device):
+    model = mod.ConvModel().to(device)
+    train_set = MNIST(".", train=True, download=True, transform=transforms.Compose([transforms.ToTensor()]))
+    test_set = MNIST(".", train=False, download=True, transform=transforms.Compose([transforms.ToTensor()]))
+    train_loader = torch.utils.data.DataLoader(train_set, shuffle=True, batch_size=args.batch_size)
+    test_loader = torch.utils.data.DataLoader(test_set, shuffle=False, batch_size=100)
+
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    criterion = nn.CrossEntropyLoss()
+    for epoch in range(args.epochs):
+        model.train()
+        print(f"Epoch #{epoch + 1}, beta: {model.beta:.3}")
+        for idx, (inputs, labels) in enumerate(train_loader):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            optimizer.zero_grad()
+            loss = criterion(model(inputs), labels)
+            loss.backward()
+            optimizer.step()
+            if idx % args.dev_every == 0:
+                print_eval(model, criterion, test_loader, device, name="test")
+        model.beta *= 0.7
+        torch.save(model.state_dict(), "mnist.pt")
+    print_eval(model, criterion, test_loader, device, name="test")
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--batch_size", default=64, type=int)
+    parser.add_argument("--lr", default=1E-3, type=float)
+    parser.add_argument("--dev_every", default=5, type=int)
+    parser.add_argument("--device", default=0, type=int, help="-1 for CPU, all else for GPU id")
+    parser.add_argument("--epochs", default=30, type=int)
+    parser.add_argument("--momentum", default=0, type=float)
+    args = parser.parse_args()
+    device = f"{'cuda' if args.device >= 0 else 'cpu'}:{args.device}"
+    device = torch.device(device)
+    run_mnist(args, device)
 
 if __name__ == "__main__":
     main()
--- concretize/model.py
+++ concretize/model.py
@@ -1,7 +1,20 @@
 import torch
+import torch.autograd as ag
 import torch.nn as nn
+import torch.nn.functional as F
 
-def binary_concrete(log_alpha, beta=1/3, gamma=-0.1, zeta=1.1, hard=False, training=False):
+class StraightThroughBernoulli(ag.Function):
+    @staticmethod
+    def forward(ctx, x):
+        return torch.bernoulli(x)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        return grad_output
+
+ste_bernoulli = StraightThroughBernoulli.apply
+
+def binary_concrete(log_alpha, beta=2/3, gamma=-0.1, zeta=1.1, hard=False, training=False, return_unif=False):
     u = torch.Tensor(*log_alpha.size()).uniform_().to(log_alpha.device)
     if training:
         s = u.log().add_((1 - u).log_().mul_(-1))
@@ -12,7 +25,32 @@ def binary_concrete(log_alpha, beta=1/3, gamma=-0.1, zeta=1.1, hard=False, train
     if hard:
         s = (zeta - gamma) * s + gamma
         s = s.clamp(0, 1)
-    return s
+    return (s, u) if return_unif else s
+
+def quantize_concrete(*args, **kwargs):
+    return 2 * binary_concrete(*args, **kwargs) - 1
+
+class ConvModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1_weight = nn.Parameter(nn.init.xavier_normal_(torch.Tensor(32, 1, 5, 5)))
+        self.conv1_bias = nn.Parameter(torch.zeros(32))
+        self.conv2_weight = nn.Parameter(nn.init.xavier_normal_(torch.Tensor(64, 32, 5, 5)))
+        self.conv2_bias = nn.Parameter(torch.zeros(64))
+        self.mp = nn.MaxPool2d(2)
+        self.lin = nn.Linear(1024, 10)
+
+        self.bn1 = nn.BatchNorm2d(32)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.beta = 2/3
+
+    def forward(self, x):
+        fn = quantize_concrete
+        x = self.mp(F.relu(F.conv2d(x, fn(self.conv1_weight, beta=self.beta), bias=fn(self.conv1_bias, beta=self.beta))))
+        x = self.bn1(x)
+        x = self.mp(F.relu(F.conv2d(x, fn(self.conv2_weight, beta=self.beta), bias=fn(self.conv2_bias, beta=self.beta))))
+        x = self.bn2(x)
+        return self.lin(x.view(x.size(0), -1))
 
 class MultiTuckerExampleModel(nn.Module):
     def __init__(self, t=(0.49,)):
